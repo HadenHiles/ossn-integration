a4281138404dc1d03bacfd8e17187fdc08e2b456
hoda <hadenhiles@gmail.com>
2020-04-23 14:52:57 -0400

    Initial commit

---
+++ includes/class-ossn-integration-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://howtohockey.com
+ * @since      1.0.0
+ *
+ * @package    Ossn_Integration
+ * @subpackage Ossn_Integration/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Ossn_Integration
+ * @subpackage Ossn_Integration/includes
+ * @author     Haden Hiles <haden@howtohockey.com>
+ */
+class Ossn_Integration_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
