d988da728e851f685dea34eff2f6ff2caa9c3f89
Haden <hadenhiles@gmail.com>
2020-05-08 10:34:02 -0400

    Grass roots plugin - start from scratch

--- ossn-integration.php
+++ ossn-integration.php
@@ -8,75 +8,20 @@
  * registers the activation and deactivation functions, and defines a function
  * that starts the plugin.
  *
- * @link              http://howtohockey.com
+ * @link              https://github.com/HadenHiles
+ * @author						Haden Hiles
  * @since             1.0.0
  * @package           Ossn_Integration
  *
  * @wordpress-plugin
  * Plugin Name:       OSSN Integation
  * Plugin URI:        ossn-integration
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Description:       This is a plugin to keep OSSN user data updated with latest wordpress info (for now just email address)
  * Version:           1.0.0
  * Author:            Haden Hiles
- * Author URI:        http://howtohockey.com
+ * Author URI:        https://github.com/HadenHiles
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       ossn-integration
  * Domain Path:       /languages
  */
-
-// If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
-	die;
-}
-
-/**
- * Currently plugin version.
- * Start at version 1.0.0 and use SemVer - https://semver.org
- * Rename this for your plugin and update it as you release new versions.
- */
-define( 'OSSN_INTEGRATION_VERSION', '1.0.0' );
-
-/**
- * The code that runs during plugin activation.
- * This action is documented in includes/class-ossn-integration-activator.php
- */
-function activate_ossn_integration() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-ossn-integration-activator.php';
-	Ossn_Integration_Activator::activate();
-}
-
-/**
- * The code that runs during plugin deactivation.
- * This action is documented in includes/class-ossn-integration-deactivator.php
- */
-function deactivate_ossn_integration() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-ossn-integration-deactivator.php';
-	Ossn_Integration_Deactivator::deactivate();
-}
-
-register_activation_hook( __FILE__, 'activate_ossn_integration' );
-register_deactivation_hook( __FILE__, 'deactivate_ossn_integration' );
-
-/**
- * The core plugin class that is used to define internationalization,
- * admin-specific hooks, and public-facing site hooks.
- */
-require plugin_dir_path( __FILE__ ) . 'includes/class-ossn-integration.php';
-
-/**
- * Begins execution of the plugin.
- *
- * Since everything within the plugin is registered via hooks,
- * then kicking off the plugin from this point in the file does
- * not affect the page life cycle.
- *
- * @since    1.0.0
- */
-function run_ossn_integration() {
-
-	$plugin = new Ossn_Integration();
-	$plugin->run();
-
-}
-run_ossn_integration();
