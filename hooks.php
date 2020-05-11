a4acb3dfab039ab4fd572601f21b6cb874888253
Haden <hadenhiles@gmail.com>
2020-05-11 16:32:12 -0400

    Revert WP user_email update if it fails to update in OSSN. Plus make the error message more clear and stop showing success message when updating fails.

--- hooks.php
+++ hooks.php
@@ -1,5 +1,5 @@
 <?php
-add_action( 'personal_options_update', 'user_profile_update', 10, 2 );
+add_action( 'profile_update', 'user_profile_update', 10, 2 );
 
 function user_profile_update( $user_id, $old_user_data ) {
     // Retrieve api key
@@ -9,29 +9,36 @@ function user_profile_update( $user_id, $old_user_data ) {
     // Retrieve updated WP user email
     $user = get_user_by('id', $user_id);
     $new_email = $user->user_email;
-    $old_email = $old_user_data->user_email;
+    $username = $old_user_data->user_login;
 
     //get the ossn user
-    $ossn_user = get_ossn_user($api_key, $old_email);
+    $ossn_user = get_ossn_user($api_key, $username);
 
     //update the ossn user email
     $update_ossn_user_response = update_ossn_user_email($api_key, $ossn_user, $new_email);
 
     if ($update_ossn_user_response->code != '100') {
-      $args = array(
-        'ID'         => $user_id,
-        'user_email' => $old_user_data->user_email
-      );
-      wp_update_user( $args );
+      global $wpdb;
+      $sql = "UPDATE hth_users SET user_email='{$old_user_data->user_email}' WHERE ID={$user_id}";
+      $results = $wpdb->get_results($sql);
+
+      $error_message = $update_ossn_user_response->message;
       ?>
+      <script type="text/javascript">
+        jQuery(window).load(function() {
+          jQuery('#mepr-account-welcome-message').nextAll('.mp_wrapper').first().children('.mepr_updated').remove();
+          jQuery('#mepr-account-welcome-message').remove();
+        });
+      </script>
       <div class="mepr_error" id="mepr_jump">
         <ul>
             <li>
-              <strong>Error</strong>: There was an error updating your email address! Please contact the <a href="mailto:haden@howtohockey.com">system administrator</a>.
+              <strong>Error</strong>: There was an error updating your email address! Please contact the <a href="mailto:haden@howtohockey.com?subject=There%20was%20an%20error%20while%20updating%20my%20email%20address&body=The%20error%20message%20was%3A%20<?=$error_message?>">system administrator</a>.
               <?php
               if (!empty($update_ossn_user_response->message)) {
                 ?>
-                <small>Message: <?=$update_ossn_user_response->message?></small>
+                <br />
+                <small style="font-size: 10px;">Error message: <?=$update_ossn_user_response->message?></small>
                 <?php
               }
               ?>
@@ -42,12 +49,12 @@ function user_profile_update( $user_id, $old_user_data ) {
     }
 }
 
-function get_ossn_user($api_key, $email) {
+function get_ossn_user($api_key, $username) {
   // get ossn user data based on email
   $curl = curl_init();
 
   curl_setopt_array($curl, array(
-    CURLOPT_URL => "https://thepond.howtohockey.com/squad/api/v1.0/user_details?api_key_token={$api_key}&email={$email}",
+    CURLOPT_URL => "https://thepond.howtohockey.com/squad/api/v1.0/user_details?api_key_token={$api_key}&username={$username}",
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_ENCODING => "",
     CURLOPT_MAXREDIRS => 10,
@@ -74,7 +81,7 @@ function update_ossn_user_email($api_key, $user, $new_email) {
   $curl = curl_init();
 
   curl_setopt_array($curl, array(
-    CURLOPT_URL => "https://thepond.howtohockey.com/squad/api/v1.0/update_user_email",
+    CURLOPT_URL => "https://thepond.howtohockey.com/squad/api/v1.0/user_update_email",
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_ENCODING => "",
     CURLOPT_MAXREDIRS => 10,
